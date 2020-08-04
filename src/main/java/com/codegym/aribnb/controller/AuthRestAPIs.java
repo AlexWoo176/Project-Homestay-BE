0a14a1ac08ced7cbf3d2754c95632b5910884aef
AlexWoo176 <phuong.tuan2012@gmail.com>
2020-08-04 08:27:06 +0700

    add DateSeeding

--- src/main/java/com/codegym/aribnb/controller/AuthRestAPIs.java
+++ src/main/java/com/codegym/aribnb/controller/AuthRestAPIs.java
@@ -11,8 +11,6 @@ import com.codegym.aribnb.repository.IRoleRepository;
 import com.codegym.aribnb.repository.IUserRepository;
 import com.codegym.aribnb.security.jwt.JwtProvider;
 import com.codegym.aribnb.security.services.UserPrinciple;
-import com.codegym.aribnb.service.IUserService;
-import com.codegym.aribnb.service.impl.RoleServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
